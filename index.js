609019b7bb486cfc2d3955a4f1bcc6c783c5638a
RRoudt <ramses@oudt.net>
2022-09-12 20:21:28 +0200

    Adding comments

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let countEl = document.getElementById("count-el")
 let totalTime = 0
 let count = 0
 
+// Increment the count by 5 and update the counter on screen
 function increment() {
     count += 5
     countEl.textContent = `${count}'`
 }
 
+// Save the current count, add it to the total time, display the total timem and reset the counter.
 function save() {
     totalTime += count;
     saveEl.textContent = `Total input time: ${totalTime}'`
@@ -15,6 +17,7 @@ function save() {
     countEl.textContent = `${count}'`
 }
 
+// Reset both the counter and total time.
 function reset() {
     count = 0
     totalTime = 0
