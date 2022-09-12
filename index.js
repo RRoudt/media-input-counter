0a0343df4c2a9232da61c4e7dea32c017df24e9f
RRoudt <ramses@oudt.net>
2022-09-12 23:42:47 +0200

    Add netTimeRatio variable and improve save button
    The counter now resets when trying to save a negative value. Also, added a netTimeRatio variable to easily set the net time multiplier.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Set 
 let totalEl = document.getElementById("total-el");
 let netEl = document.getElementById("net-el");
 let countEl = document.getElementById("count-el");
 let timesSavedEl = document.getElementById("times-saved-el");
-let timesSaved = 0;
-let totalTime = 0;
-let netTime = 0;
-let count = 0;
+
+// Set empty counters for different functionality
+let timesSaved = 0; // Counts how many times the SAVE button was clicked
+let totalTime = 0; // Counts how much total time was saved
+let netTime = 0; // Counts how much net time was saved 
+let netTimeRatio = 0.67; // Sets ratio between total time and net time
+let count = 0; // The live counter which is used to save the total time
 
 // Increment counter by 5 minutes and update HTML element
 function increment() {
@@ -21,13 +25,20 @@ function decrement() {
 
 // Save counter to total time, if the counter is above 0. Also updates the relevant HTML elements
 function save() {
+    
+    // Check if counter is above 0 before saving, to avoid getting a negative total input time
     if (count > 0) {
         totalTime += count;
         timesSaved += 1;
-        netTime = Math.floor(totalTime * 0.67);
+        netTime = Math.floor(totalTime * netTimeRatio);
         totalEl.textContent = `Total input time: ${totalTime}'`;
         netEl.textContent = `Net input time: ${netTime}'`;
         timesSavedEl.textContent = `Times saved: ${timesSaved}`;
+        count = 0;
+        countEl.textContent = `${count}'`;
+    } 
+    // Reset counter if it's negative, but don't save it.
+    else {
         count = 0;
         countEl.textContent = `${count}'`;
     }
