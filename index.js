d5153cde166df8b3a00f005f6a64d949d27aaec3
RRoudt <ramses@oudt.net>
2022-09-12 22:26:28 +0200

    Adding functions to calculate net time (2/3 total)

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let totalTime = 0
-let count = 0
+let totalEl = document.getElementById("total-el");
+let netEl = document.getElementById("net-el");
+let countEl = document.getElementById("count-el");
+let totalTime = 0;
+let netTime = 0;
+let count = 0;
 
-// Increment the count by 5 and update the counter on screen
 function increment() {
-    count += 5
-    countEl.textContent = `${count}'`
+    count += 5;
+    countEl.textContent = `${count}'`;
 }
 
-// Save the current count, add it to the total time, display the total timem and reset the counter
 function save() {
     totalTime += count;
-    saveEl.textContent = `Total input time: ${totalTime}'`
-    count = 0
-    countEl.textContent = `${count}'`
+    netTime = Math.floor(totalTime * 0.67);
+    totalEl.textContent = `Total input time: ${totalTime}'`;
+    netEl.textContent = `Net input time: ${netTime}'`;
+    count = 0;
+    countEl.textContent = `${count}'`;
 }
 
-// Reset both the counter and total time
 function reset() {
-    count = 0
-    totalTime = 0
-    saveEl.textContent = "Total input time: "
+    count = 0;
+    totalTime = 0;
+    totalEl.textContent = "Total input time: ";
+    netEl.textContent = "Net input time: ";
 }
